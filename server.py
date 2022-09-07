00e054e37fcc0d992aa0823b08840376daf7c14f
Winnie <winiigboama@gmail.com>
2022-09-07 21:36:36 +0100

    feat: Do not start start_game_when_two_clients if the clients are two
    - Add new condition for when start_game_when_two_clients thread should run in remove_client - when length of clients is not two
    - If a client has already been removed, and they no longer exist in the clients list, catch the exception when they are tried to be removed again and do nothing instead
    - Handle exception that occurs when send_data in server.py, raise it, let it bubble out and handle it in play_game thread instead

--- server.py
+++ server.py
@@ -56,7 +56,7 @@ class Server:
         try:
             conn.send(data)
         except socket.error as e:
-            print(f"Error sending data: {e}")
+            raise
 
 
     def start(self):
@@ -121,11 +121,17 @@ class Server:
         global clients
         print("Lost connection")
         print(f"[DISCONNECTION] {addr} disconnected.")
-        clients.remove((conn, addr))        
-        conn.close()
+        try:
+            clients.remove((conn, addr))
+        except ValueError:
+            pass
+        try:      
+            conn.close()
+        except socket.error:
+            raise
 
         # Listen for new connection(s) after losing client only if the thread is not already running and if start_new_game is True
-        if not self.start_game_when_two_clients_thread.is_alive() and start_new_game:
+        if not self.start_game_when_two_clients_thread.is_alive() and start_new_game and len(clients) != 2:
             self.start_game_when_two_clients_thread = threading.Thread(target=self.start_game_when_two_clients)
             self.start_game_when_two_clients_thread.start()
                 
@@ -150,98 +156,112 @@ class Server:
 
         while True:
             try:
-                msg = conn.recv(16)   
-            except ConnectionResetError as e: #  This exception is caught when the client tries to receive a msg from a disconnected client
-                print(f"Connection Reset: {e}")
-                if conn == conn1:
-                    self.send_data(conn2, {"other_client_disconnected":"Other client disconnected unexpectedly"})
-                else:
-                    self.send_data(conn1, {"other_client_disconnected":"Other client disconnected unexpectedly"})
-                self.remove_client(conn, addr)
-                break
-            except socket.error as e:
-                print(f"Error receiving data: {e}")
-                            
-
-            if not msg:
-                self.remove_client(conn, addr)
-                break
-
-            if new_msg:
-                msglen = int(msg[:self.HEADERSIZE])
-                new_msg = False
-
-
-            full_msg += msg
-
-            if len(full_msg)-self.HEADERSIZE == msglen:
-                # ----------------Use loaded json data here----------------
-
-                loaded_json = pickle.loads(full_msg[self.HEADERSIZE:])
-                # print("loaded_json: ",  loaded_json)
-                new_msg = True
-                full_msg = b''     
                 try:
-                    if 'id' in loaded_json:
-                        id = loaded_json['id']
-                        if not id: #  If id is 0 i.e. if first connected player...
-                            self.send_data(conn, {"get-first-player-name":True})
-                        else:
-                            self.send_data(conn, {"waiting-for-name":"Waiting for other player to enter their name"})
-                    elif 'you' in loaded_json:
-                        you = loaded_json['you']
-                        if conn == conn1:
-                            self.send_data(conn2, {'opponent':you})
-                        elif conn == conn2:
-                            self.send_data(conn1, {'opponent':you})                        
-                    elif 'first' in loaded_json:
-                        self.send_data(conn1, {'first':loaded_json['first']})                        
-                        self.send_data(conn2, {'first':loaded_json['first']})
-                    elif 'colors' in loaded_json:
-                        self.send_data(conn1, {'colors':loaded_json['colors']})
-                        self.send_data(conn2, {'colors':loaded_json['colors']})
-                    elif 'opponent_player_object' in loaded_json:
-                        if conn == conn1:
-                            self.send_data(conn2, {'opponent_player_object':loaded_json['opponent_player_object']})
-                        elif conn == conn2:
-                            self.send_data(conn1, {'opponent_player_object':loaded_json['opponent_player_object']})                                            
-                    elif 'board' in loaded_json:
-                        if conn == conn1:
-                            self.send_data(conn2, {'board':loaded_json['board']})                            
-                        elif conn == conn2:
-                            self.send_data(conn1, {'board':loaded_json['board']})  
-                    elif 'round_over' in loaded_json:
-                        self.send_data(conn1, loaded_json)
-                        self.send_data(conn2, loaded_json)
-                    elif 'play_again' in loaded_json:
-                        try:
-                            if conn == conn1:
-                                self.send_data(conn2, {'play_again':loaded_json['play_again']})
-                            elif conn == conn2:
-                                self.send_data(conn1, {'play_again':loaded_json['play_again']})
-                        except OSError: # Other client may have disconnected already so sending data to it will raise OSError exception
-                            continue
-                    elif 'first_player' in loaded_json:                        
-                        self.send_data(conn1, {'first_player':loaded_json['first_player']})                        
-                        self.send_data(conn2, {'first_player':loaded_json['first_player']})
-                    elif 'wait_for_new_client' in loaded_json:
-                        self.start_game_when_two_clients_thread = threading.Thread(target=self.start_game_when_two_clients)
-                        self.start_game_when_two_clients_thread.start()
-                        break
-                    elif 'other_client_disconnected' in loaded_json:
-                        break
-                    elif 'DISCONNECT' in loaded_json:
-                        if loaded_json['DISCONNECT'] == self.DISCONNECT_MESSAGE:                            
-                            if 'disconnect_sent_to_end_game' in loaded_json and len(clients) == 2:
-                                self.remove_client(conn, addr, start_new_game=False)
-                            else:
-                                self.remove_client(conn, addr)
-                            break
-                except KeyError:
+                    msg = conn.recv(16)   
+                except ConnectionResetError as e: #  This exception is caught when the client tries to receive a msg from a disconnected client
+                    print(f"Connection Reset: {e}")
+                    if conn == conn1:
+                        self.send_data(conn2, {"other_client_disconnected":"Other client disconnected unexpectedly"})
+                    else:
+                        self.send_data(conn1, {"other_client_disconnected":"Other client disconnected unexpectedly"})
                     self.remove_client(conn, addr)
-                    break               
-                        # ----------------Use loaded json data here----------------
-            
+                    break
+                except socket.error as e:
+                    print(f"Error receiving data: {e}")
+                    if conn == conn1:
+                        self.send_data(conn2, {"other_client_disconnected":"An error occured with the other client"})
+                    else:
+                        self.send_data(conn1, {"other_client_disconnected":"An error occured with the other client"})
+                    self.remove_client(conn, addr)
+                    break
+                                
+
+                if not msg:
+                    self.remove_client(conn, addr)
+                    break
+
+                if new_msg:
+                    msglen = int(msg[:self.HEADERSIZE])
+                    new_msg = False
+
+
+                full_msg += msg
+
+                if len(full_msg)-self.HEADERSIZE == msglen:
+                    # ----------------Use loaded json data here----------------
+
+                    loaded_json = pickle.loads(full_msg[self.HEADERSIZE:])
+                    # print("loaded_json: ",  loaded_json)
+                    new_msg = True
+                    full_msg = b''     
+                    try:
+                        if 'id' in loaded_json:
+                            id = loaded_json['id']
+                            if not id: #  If id is 0 i.e. if first connected player...
+                                self.send_data(conn, {"get-first-player-name":True})
+                            else:
+                                self.send_data(conn, {"waiting-for-name":"Waiting for other player to enter their name"})
+                        elif 'you' in loaded_json:
+                            you = loaded_json['you']
+                            if conn == conn1:
+                                self.send_data(conn2, {'opponent':you})
+                            elif conn == conn2:
+                                self.send_data(conn1, {'opponent':you})                        
+                        elif 'first' in loaded_json:
+                            self.send_data(conn1, {'first':loaded_json['first']})                        
+                            self.send_data(conn2, {'first':loaded_json['first']})
+                        elif 'colors' in loaded_json:
+                            self.send_data(conn1, {'colors':loaded_json['colors']})
+                            self.send_data(conn2, {'colors':loaded_json['colors']})
+                        elif 'opponent_player_object' in loaded_json:
+                            if conn == conn1:
+                                self.send_data(conn2, {'opponent_player_object':loaded_json['opponent_player_object']})
+                            elif conn == conn2:
+                                self.send_data(conn1, {'opponent_player_object':loaded_json['opponent_player_object']})                                            
+                        elif 'board' in loaded_json:
+                            if conn == conn1:
+                                self.send_data(conn2, {'board':loaded_json['board']})                            
+                            elif conn == conn2:
+                                self.send_data(conn1, {'board':loaded_json['board']})  
+                        elif 'round_over' in loaded_json:
+                            self.send_data(conn1, loaded_json)
+                            self.send_data(conn2, loaded_json)
+                        elif 'play_again' in loaded_json:
+                            try:
+                                if conn == conn1:
+                                    self.send_data(conn2, {'play_again':loaded_json['play_again']})
+                                elif conn == conn2:
+                                    self.send_data(conn1, {'play_again':loaded_json['play_again']})
+                            except OSError: # Other client may have disconnected already so sending data to it will raise OSError exception
+                                continue
+                        elif 'first_player' in loaded_json:                        
+                            self.send_data(conn1, {'first_player':loaded_json['first_player']})                        
+                            self.send_data(conn2, {'first_player':loaded_json['first_player']})
+                        elif 'wait_for_new_client' in loaded_json:
+                            self.start_game_when_two_clients_thread = threading.Thread(target=self.start_game_when_two_clients)
+                            self.start_game_when_two_clients_thread.start()
+                            break
+                        elif 'other_client_disconnected' in loaded_json:
+                            break
+                        elif 'DISCONNECT' in loaded_json:
+                            if loaded_json['DISCONNECT'] == self.DISCONNECT_MESSAGE:                            
+                                if 'disconnect_sent_to_end_game' in loaded_json and len(clients) == 2:
+                                    self.remove_client(conn, addr, start_new_game=False)
+                                else:
+                                    self.remove_client(conn, addr)
+                                break
+                    except KeyError:
+                        self.remove_client(conn, addr)
+                        break               
+                            # ----------------Use loaded json data here----------------
+            except socket.error as e:
+                print(f"Error sending data: {e}")
+                if conn == conn1:
+                    self.send_data(conn2, {"other_client_disconnected":"An error occured with the other client"})
+                else:
+                    self.send_data(conn1, {"other_client_disconnected":"An error occured with the other client"})
+                self.remove_client(conn, addr)
+                break
 
 
         
