a905bf3579f59e7f840e281d4bc0d6a53d668e70
Winnie <winiigboama@gmail.com>
2022-10-11 03:15:21 +0100

    fix: Add removed JOIN_GAME_WITH_ENTERED_CODE in GameState
    - Add removed JOIN_GAME_WITH_ENTERED_CODE in GameState as it is the action of the join-game btn in the join_game_with_code screen

--- src/pygame_version/gamestate.py
+++ src/pygame_version/gamestate.py
@@ -7,8 +7,9 @@ class GameState(Enum):
     CREATE_GAME = 1
     JOIN_ANY_GAME = 2
     JOIN_GAME_WITH_CODE = 3
-    COPY = 4
-    PASTE = 5
-    CONTINUE = 6
-    CONTINUE_WITH_DEFAULT_IP = 7
-    HELP = 8
+    JOIN_GAME_WITH_ENTERED_CODE = 4
+    COPY = 5
+    PASTE = 6
+    CONTINUE = 7
+    CONTINUE_WITH_DEFAULT_IP = 8
+    HELP = 9
